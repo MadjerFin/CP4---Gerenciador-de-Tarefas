cd8d22736894465cdc4779a990d2ee347d63d959
MadjerFin <madjer.finamor@gmail.com>
2025-03-06 13:43:31 -0300

    capturar elementos do DOM

--- script.js
+++ script.js
@@ -2,3 +2,13 @@
 let tarefas = [
     { ...{ id: 1, titulo: 'Tarefa inicial de exemplo', concluida: false } }
 ];
+
+
+// Capturando elementos do DOM
+const formTarefa = document.getElementById('formTarefa');
+const entradaTarefa = document.getElementById('entradaTarefa');
+const botaoAdicionarTarefa = document.getElementById('botaoAdicionarTarefa');
+const listaTarefas = document.getElementById('listaTarefas');
+const botaoFiltrarPendentes = document.getElementById('botaoFiltrarPendentes');
+const botaoMostrarTodas = document.getElementById('botaoMostrarTodas');
+
