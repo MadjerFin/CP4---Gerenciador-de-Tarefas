31e44b8b73a94e8aa73c1f243a2eee202bfae477
MadjerFin <madjer.finamor@gmail.com>
2025-03-06 13:46:31 -0300

    questão 2 / 3

--- script.js
+++ script.js
@@ -12,3 +12,35 @@ const listaTarefas = document.getElementById('listaTarefas');
 const botaoFiltrarPendentes = document.getElementById('botaoFiltrarPendentes');
 const botaoMostrarTodas = document.getElementById('botaoMostrarTodas');
 
+
+// questão2: Evento para capturar o valor do input e criar nova tarefa
+formTarefa.addEventListener('submit', (evento) => {
+    // Prevenir o comportamento padrão do formulário (recarregar a página)
+    evento.preventDefault();
+    
+    const titulo = entradaTarefa.value.trim();
+    
+    if (titulo) {
+        // Criação de nova tarefa
+        const novaTarefa = {
+            id: Date.now(),
+            titulo: titulo,
+            concluida: false
+        };
+        
+        // Adicionar ao array
+        tarefas.push(novaTarefa);
+        
+        // Questão 3: Alert informando que a tarefa foi adicionada
+        alert('Tarefa adicionada com sucesso!');
+        
+        // Limpar input
+        entradaTarefa.value = '';
+        
+        // Atualizar a lista de tarefas
+        renderizarTarefas();
+        
+    
+    }
+});
+
